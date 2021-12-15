82d6b881032b02059bef004c8b06589b51f02f39
lhwarthas <lhwarthas@163.com>
2021-12-15 10:59:10 +0800

    update kernel version to 5.4.165

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,7 +20,6 @@
 
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git dockerman https://github.com/lisaac/luci-app-dockerman' >>feeds.conf.default
 echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic' >>feeds.conf.default
 
 #add luci-app-cpufreq
